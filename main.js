a61a4d3c97c6d444436dd709dcb08c040a9f552b
Thomas Urban <thomas.urban@cepharum.de>
2017-08-12 15:34:06 +0200

    revising code: keep links in window, auto-save any download on Calliope

--- main.js
+++ main.js
@@ -28,19 +28,26 @@
 
 "use strict";
 
-const { app: App, BrowserWindow } = require( "electron" );
+const { app: App, BrowserWindow, dialog: Dialog } = require( "electron" );
 
 const Path = require( "path" );
 const Url = require( "url" );
 
+const Calliope = require( "./lib/calliope" );
+
 
 let win;
 
-function createWindow() {
+function createWindow( calliope ) {
 	win = new BrowserWindow( {
 		width: 800,
 		height: 600,
 		show: false,
+	} );
+
+	Dialog.showMessageBox( win, {
+		message: calliope.detailsFilename,
+		title: calliope.pathname,
 	} );
 
 	win.setMenu( null );
@@ -59,9 +66,27 @@ function createWindow() {
 	win.on( "closed", () => {
 		win = null;
 	} );
+
+	win.webContents.session.on( "will-download", ( event, item ) => {
+		item.setSavePath( Path.resolve( calliope.pathname, "mini.hex" ) );
+	} );
+
+	win.webContents.on( "new-window", ( event, url ) => {
+		event.preventDefault();
+		win.loadURL( url );
+	} );
 }
 
-App.on( "ready", createWindow );
+App.on( "ready", () => {
+	Calliope.find()
+		.then( createWindow, error => {
+			Dialog.showMessageBox( {
+				type: "error",
+				message: error.message,
+				title: "Starting Calliope Editor failed.",
+			}, () => App.quit() );
+		} );
+} );
 
 App.on( "window-all-closed", () => {
 	if ( process.platform !== "darwin" ) {
